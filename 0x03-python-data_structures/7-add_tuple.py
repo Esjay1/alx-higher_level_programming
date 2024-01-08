ae46cca2a92ab9b85331878bf1cc991417203480
Sijibomi Olaiya <sijiolaiya@gmail.com>
2024-01-08 12:17:50 -0800

    a function that adds 2 first values in 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    newtuplea = list(tuple_a)
+    newtupleb = list(tuple_b)
+    if len(newtuplea) == 0:
+        newtuplea.append(0)
+        newtuplea.append(0)
+    if len(newtupleb) == 0:
+        newtupleb.append(0)
+        newtupleb.append(0)
+    if len(newtuplea) < 2:
+        newtuplea.append(0)
+    if len(newtupleb) < 2:
+        newtupleb.append(0)
+    resulttuple = [newtuplea[0] + newtupleb[0], newtuplea[1] + newtupleb[1]]
+    resulttuple1 = tuple(resulttuple)
+    return resulttuple1
+
+
+
