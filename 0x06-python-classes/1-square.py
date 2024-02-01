34d7c7afe43e6a156464d19cd229e2d202d204a9
Sijibomi Olaiya <sijiolaiya@gmail.com>
2024-02-01 07:36:00 -0800

    class square with a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This task creates an empty class"""
+class Square:
+    """This class defines what a Square is
+    but the task requres me to create an empty
+    one for now.
+    
+    Attributes:
+        size: to be a private attribute
+
+    Methods:
+        No methods
+
+    Examples:
+        No example yet
+
+    """
+    def __init__(self, size):
+        """This defines the initial values"""
+        self.__size = size
