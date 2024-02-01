95aecc6350a9fa298523f4a6a02ee5285df5f91e
Sijibomi Olaiya <sijiolaiya@gmail.com>
2024-02-01 07:39:46 -0800

    class square with a private attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This task creates an empty class"""
+
+
 class Square:
     """This class defines what a Square is
     but the task requres me to create an empty
     one for now.
-    
+
     Attributes:
         size: to be a private attribute
 
