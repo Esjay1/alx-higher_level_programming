f4a51308248410230ab5b88f10dbb186a169c71c
Sijibomi Olaiya <sijiolaiya@gmail.com>
2024-02-01 00:31:02 -0800

    empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This class defines what a square is.
+"""
+class Square:
+    """This is the area where it will be defined
+    but the task requres me to create an empty
+    one for now.
+    """
+    pass
