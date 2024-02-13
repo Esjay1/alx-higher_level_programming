ce28a1b74edf7294fef01881057711e0390d50a7
Sijibomi Olaiya <sijiolaiya@gmail.com>
2024-02-13 04:29:43 -0800

    class Rectangle that defines a rectangle by private attributes width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,20 +8,6 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-    """Width setter for width"""
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if type(value) != (int or float):
-            raise TypeError('width must be an integer')
-        if value < 0:
-            raise ValueError('width must be >= 0')
-        self.__width = value
-
-    """height setter for height"""
     @property
     def height(self):
         return self.__height
@@ -33,3 +19,15 @@ class Rectangle:
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != (int or float):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
