2e52965e9937ba9a156ad3b89edde395d71a3ddf
Sijibomi Olaiya <sijiolaiya@gmail.com>
2024-03-17 08:36:29 -0700

    dislay the average score

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,5 +1,5 @@
 -- Thos scriptvcomputes the average of the values
 -- of the scores column in the second_table
 -- table of the hbtn_0c_0 database
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
 
