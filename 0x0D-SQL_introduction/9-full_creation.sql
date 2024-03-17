cdecd11fa62f80fb4fc725bc501562baecde65d8
Sijibomi Olaiya <sijiolaiya@gmail.com>
2024-03-17 07:27:21 -0700

    create a table 'second_table' in the database 'hbtn_0c_0' in the MySQL server and add multiple rows into it

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,5 @@
 -- This script creates a table second_table in the database hbtn_0c_0
-DROP TABLE IF EXISTS second_table;
-CREATE TABLE second_table(
+CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR(256),
 	score INT);
