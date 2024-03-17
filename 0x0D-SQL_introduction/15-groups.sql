10a3666733a3341393c818d6128982c0b326223d
Sijibomi Olaiya <sijiolaiya@gmail.com>
2024-03-17 12:44:00 -0700

    list the number of records with the same score in the second_table table of the hbtn_0c_0 database

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- This script lists the number of records with the
+-- same score in the second_table table of the
+-- hbtn_0c_0 database.
+SELECT score, COUNT(*) AS number FROM second_table
+GROUP BY score
+ORDER BY number DESC;
+
