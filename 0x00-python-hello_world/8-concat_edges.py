e4605db845b5077a6a6f2f8b64581f8accaf7789
Sijibomi Olaiya <sijiolaiya@gmail.com>
2024-05-26 07:21:39 -0700

    print some parts of a string variable using positioning

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
     language that combines remarkable power with very clear syntax"
-print(f'{str[39:66]} {str[110:114]} {str[0:6]}', end=' ')
+print(str[39:67] + str[110:115] + str[0:6])
 print('')
